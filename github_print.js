049dcc2331a6fa63c7755bd218dbb49b7a3aa0a4
niris <smresc@gmail.com>
2012-09-12 17:09:19 +0800

    github_print. fix bug. did not hide element

--- github_print.js
+++ github_print.js
@@ -2,20 +2,19 @@ javascript: (function() {
 	/*彩色打印*/
 	var css = document.styleSheets;
 	for (var i = css.length - 1; i >= 0; --i) {
-		css[i].media.mediaText += ", print";
+		css[i].media.mediaText = '';
 	}
 	/*隐藏无需打印的元素*/
-	var hide = ["#header", "#footer-push", ".pagehead", "#slider .breadcrumb", ".commit", ".meta", "#footer"];
-	hide.forEach(function(i) {
-		document.querySelector(i).style.display = "none";
-	});
-	/*样式调整*/
-	document.querySelector(".site").style.padding = "0";
-	document.querySelector("#files").style.padding = "0";
-	var fontStyle = "14px DejaVu Sans Mono";
-	document.querySelector(".line_numbers").style.font = fontStyle;
-	var code = document.querySelectorAll(".line");
-	for (var i = code.length - 1; i >= 0; --i) {
-		code[i].style.font = fontStyle;
+	var hide = ['#header', '#footer-push', '.pagehead', '.breadcrumb', '.commit', '.meta', '#footer'];
+	var node = document.querySelectorAll(hide.join(','));
+	for (var i = node.length - 1; i >= 0; --i) {
+		node[i].style.display = 'none';
+	}
+	document.querySelector('.site').style.padding = '0';
+	document.querySelector('#files').style.padding = '0';
+	var lines = document.querySelectorAll('.line, .line_numbers');
+	var fontStyle = '14px DejaVu Sans Mono';
+	for (var i = lines.length - 1; i >= 0; --i) {
+		lines[i].style.font = fontStyle;
 	}
 })()
