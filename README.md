06ae42b4bee84324cd39828dcc4a5cc37ba02c3d
niris <smresc@gmail.com>
2012-11-09 16:57:41 +0800

    no more template.js

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 + ass2srt.py 把 ass 字幕转换成 srt 字幕。
 + escape.py 转义 html 字符。
 + github_print.js 打印 github 的代码用的 bookmarklet。
++ template.js 一个练手写的的 js 模板，没啥用，有机会再改进。
 + verycd.js 输出 verycd 页面的全部 ed2k 链接，现在没什么用了……
-+ template.js 一个很随手的 js 模板。
--- template.js
+++ template.js
@@ -1,95 +1,39 @@
 /*
  * Usage:
- * var t = template('{{ ns.key }}');
- * var outpot = t.generate({'key': 'value');
+ * var output = tmpl('id', 'data');
+ * var output = tmpl('id').generate('data');
+ * var output = tmpl.template('string').generate('data');
  *
  * Syntax:
  * {{ expr }}
  * {% if (...) %}... {% else if (...) %}... {% else %}... {% end %}
- * {% for (...;...;...) %}... {% end %}
+ * {% for (...; ...; ...) %}... {% end %}
  */
 
 (function() {
 	'use strict';
-	var compile = function(str) {
-		var compiled = '"use strict";var o="';
-		compiled += str.replace(/\s+/g, ' ')
-			.replace(/{{\s?/g, '"+')
-			.replace(/\s?}}/g, '+"')
+
+	var template = function(str) {
+		var compiled = '"use strict";var output="' +
+			str.replace(/\s+/g, ' ')
+			.replace(/{{/g, '"+')
+			.replace(/}}/g, '+"')
 			.replace(/{%\s?else/g, '";}else')
-			.replace(/{%\s?end\s?%}/g, '";}o+="')
+			.replace(/{%\s?end\s?%}/g, '";}output+="')
 			.replace(/{%/g, '";')
-			.replace(/%}/g, '{o+="')
-		compiled += '";return o;';
-		console.log(compiled);
-		return compiled;
+			.replace(/%}/g, '{output+="') +
+			'";return output;';
+		//console.log(compiled);
+		return {'generate': new Function('ns', compiled)};
 	};
-	
-	this.template = function(str) {
-		return {'generate': new Function('ns', compile(str))};
+
+	var cache = Object.create(null);
+	var tmpl = function(id, data) {
+		if (! (id in cache))
+			cache[id] = template(document.getElementById(id).innerHTML);
+		return data ? cache[id].generate(data) : cache[id];
 	};
+	tmpl.template = template;
+
+	this.tmpl = tmpl;
 }).call(window);
-
-var test = function() {
-	function equal(input, output, data) {
-		var o = template(input).generate(data || {});
-		if (o === output) {
-			console.log('PASS');
-			console.log('\n');
-		} else {
-			console.log('FAIL');
-			console.log('input: ' + input);
-			console.log('except: ' + output);
-			console.log('output: ' + o);
-			console.log('\n');
-		}
-	}
-
-	// expression test
-	var et = function() {
-		var oo = 'hello, world!';
-		equal('hello, world!', oo);
-		equal('hello, {{ ns.world }}!', oo, {'world':'world'});
-		equal('hello, {{ ns.data[0] }}!', oo, {'data': ['world']});
-		equal('hello, {{ ns.data.world }}!', oo, {'data': {'world':'world'}});
-		equal('hello, {{ ns.data.world.w }}!', oo, {'data': {'world':{'w':'world'}}});
-		equal("hello, {{ ns.data['world'] }}!", oo, {'data': {'world':'world'}});
-		equal('hello, {{ ns.data["world"] }}!', oo, {'data': {'world':'world'}});
-	};
-
-	// if statement test
-	var ift = function() {
-		var ii = '{%   if (  ns.s   ==1) %}one' +
-			'{% else if  ( ns.s===2) %}two' +
-			'{% else   if( ns.s ==3)%}three' +
-			'{%   else if (ns.s== 4)%}four' +
-			'{%   else   if (ns.s<6 )  %}five' +
-			'{%   else   if( ns.s<=6)   %}six' +
-			'{% else if (ns.s<= 7) %}seven' +
-			'{% else if (ns.s !== 9) %}eight' +
-			'{% else %}nine{%end%}';
-		equal(ii, 'one', {'s': 1});
-		equal(ii, 'two', {'s': 2});
-		equal(ii, 'three', {'s': 3});
-		equal(ii, 'four', {'s': 4});
-		equal(ii, 'five', {'s': 5});
-		equal(ii, 'six', {'s': 6});
-		equal(ii, 'seven', {'s': 7});
-		equal(ii, 'eight', {'s': 8});
-		equal(ii, 'nine', {'s': 9});
-	};
-	
-	// for loop test
-	var ft = function() {
-		var ii = '{% for (var i=0,l=ns.l.length;i<l;i++) %}' +
-			'<li>{{ ns.l[i] }}</li>' +
-			'{% end %}';
-		equal(ii, '<li>1</li><li>2</li>', {'l':[1,2]});
-	};
-	
-	
-	//et();
-	//ift();
-	ft();
-};
-test();
