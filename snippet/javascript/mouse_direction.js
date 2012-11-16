974c12a940e409979e4af1786e81d0e52926834d
niris <smresc@gmail.com>
2012-11-16 19:49:43 +0800

    mouse_direction | fix bug

--- snippet/javascript/mouse_direction.js
+++ snippet/javascript/mouse_direction.js
@@ -14,32 +14,23 @@
 (function() {
 	'use strict';
 
-	var cache = Object.create(null);
 	this.getDirection = function(elem, event) {
-		if (!(elem in cache)) {
-			var pos = elem.getBoundingClientRect();
-			cache[elem] = {
-				'x': pos.left + pos.width / 2,
-				'y': pos.top + pos.height / 2
-			};
-			if (pos.width > pos.height) {
-				cache[elem].nx = pos.width / pos.height;
-				cache[elem].ny = 1;
-			} else {
-				cache[elem].nx = 1;
-				cache[elem].ny = pos.height / pos.width;
-			}
+		var pos = elem.getBoundingClientRect();
+		var nx = 1;
+		var ny = 1;
+		if (pos.width > pos.height) {
+			nx = pos.height / pos.width;
+		} else {
+			ny = pos.width / pos.height;
 		}
-
-		var o = cache[elem];
-		var x = (event.pageX - o.x) * o.nx;
-		var y = (event.pageY - o.y) * o.ny;
-		var degrees = Math.atan2(y, x) * 57.3; // (180 / Math.PI)
-		var direction = undefined;
-		if (degrees > 135) direction = 3;
-		else if (degrees > 45) direction = 2;
-		else if (degrees > -45) direction = 1;
-		else if (degrees > -135) direction = 0;
+		var x = (event.clientX - pos.left - pos.width / 2) * nx;
+		var y = (event.clientY - pos.top - pos.height / 2) * ny;
+		var degree = Math.atan2(y, x) * 180 / Math.PI;
+		var direction = 0;
+		if (degree > 135) direction = 3;
+		else if (degree > 45) direction = 2;
+		else if (degree > -45) direction = 1;
+		else if (degree > -135) direction = 0;
 		else direction = 3;
 		return direction;
 	};
