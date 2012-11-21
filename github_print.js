948dc0fed374cc53f00e32e206903ae44b747ada
niris <nirisix@gmail.com>
2012-11-21 19:04:48 +0800

    github_print.js update

--- github_print.js
+++ github_print.js
@@ -1,15 +1,19 @@
-javascript: (function() {
+//javascript:(function(){for(var b=document.styleSheets,a=b.length-1;0<=a;--a)b[a].media.mediaText="";b=document.querySelectorAll(".header,#footer-push,.pagehead,.breadcrumb,.commit,.meta,#footer");for(a=b.length-1;0<=a;--a)b[a].style.display="none";document.querySelector(".site").style.padding="0";document.querySelector("#files").style.padding="0";b=document.querySelectorAll(".line, .line_numbers");for(a=b.length-1;0<=a;--a)b[a].style.font="14px DejaVu Sans Mono"})();
+
+(function() {
 	/*彩色打印*/
 	var css = document.styleSheets;
 	for (var i = css.length - 1; i >= 0; --i) {
 		css[i].media.mediaText = '';
 	}
+	
 	/*隐藏无需打印的元素*/
-	var hide = ['#header', '#footer-push', '.pagehead', '.breadcrumb', '.commit', '.meta', '#footer'];
+	var hide = ['.header', '#footer-push', '.pagehead', '.breadcrumb', '.commit', '.meta', '#footer'];
 	var node = document.querySelectorAll(hide.join(','));
 	for (var i = node.length - 1; i >= 0; --i) {
 		node[i].style.display = 'none';
 	}
+	
 	/*调整样式*/
 	document.querySelector('.site').style.padding = '0';
 	document.querySelector('#files').style.padding = '0';
