c6d3fa66db9d224e7c9e033b28f0efda30cc49b4
niris <nirisix@gmail.com>
2012-12-19 16:24:51 +0800

    type.js | add usage

--- snippet/javascript/type.js
+++ snippet/javascript/type.js
@@ -1,6 +1,9 @@
 /*
  * use `Object.prototype.toString` to detect object type.
  *
+ * usage:
+ * >>> var t = type(object);
+ *
  * `new Date()` is Date object, but `Date()` is String object.
  * `NaN` and `Infinity` are Number object.
  *
@@ -14,8 +17,10 @@
 	this.type = function(o) {
 		var t = toString.call(o);
 		if (t === '[object Number]') {
-			if (isNaN(o)) return 'NaN';
-			else if (! isFinite(o)) return 'Infinity';
+			if (isNaN(o))
+				return 'NaN';
+			else if (!isFinite(o))
+				return 'Infinity';
 		}
 		return t.slice(8, -1);
 	};
