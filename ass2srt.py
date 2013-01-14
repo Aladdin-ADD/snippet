2015a02fc9fb8c95462a96beb471727c86d693d5
niris <nirisix@gmail.com>
2013-01-14 14:14:09 +0800

    add output for ass2srt.py

--- ass2srt.py
+++ ass2srt.py
@@ -9,46 +9,61 @@ import re
 
 def convert(ass_file, srt_file):
     re_ass = re.compile(
-        r"""(?ix)
+        r'''(?ix)               # get time and subtitle
         Dialogue:\s\d,
-        (\d+:\d\d:\d\d.\d\d),   # start
-        (\d+:\d\d:\d\d.\d\d),   # end
+        (\d+:\d\d:\d\d.\d\d),   # start time
+        (\d+:\d\d:\d\d.\d\d),   # end time
         (?:[^,]*,){6}
-        (.*)$                   # text
-        """)
-    re_newline = re.compile(r"""(?i)\n""")  # replace \N
-    re_special = re.compile(r"(?x) \{ [^}]+ \}")  # replace style
+        (.*)$                   # subtitle
+        ''')
+    re_newline = re.compile(r'(?i)\n') # replace \N with newline
+    re_style = re.compile(r'(?x) \{ [^}]+ \}') # replace style
 
-    def convert_line(ass_line, line_cnt=[-1], CONN=' --> ', LF='\n'):
+    COUNT = -1 # subtitle number
+    CONN = ' --> '
+    LF = '\n'
+
+    def convert_line(ass_line):
         m = re_ass.search(ass_line)
-        if not m: return ''
-        line_cnt[0] += 1
-        text = re_special.sub('', m.group(3))
-        text = re_newline.sub('\n', text)
-        return (str(line_cnt[0]) + LF +
+
+        if not m: # line without subtitle
+            return ''
+
+        nonlocal COUNT
+        COUNT += 1
+        subtitle = re_style.sub('', m.group(3))
+        subtitle = re_newline.sub('\n', subtitle)
+        return (str(COUNT) + LF +
                 m.group(1) + CONN + m.group(2) + LF +
-                text + LF + LF)
+                subtitle + LF + LF)
 
     with open(ass_file) as ass:
         with open(srt_file, 'w') as srt:
             srt.writelines([convert_line(line) for line in ass])
 
+    print(ass_file, '-->', srt_file)
+
+
+
 
 def main():
-    if len(sys.argv) == 2:
+    l = len(sys.argv)
+    if l == 2:
         ass = sys.argv[1]
-        if ass.endswith(".ass"):
-            srt = ass.replace(".ass", ".srt")
+        if ass.endswith('.ass'):
+            srt = ass.replace('.ass', '.srt')
         else:
-            srt = ass + ".srt"
-    elif len(sys.argv) == 3:
+            srt = ass + '.srt'
+    elif l == 3:
         ass = sys.argv[1]
         srt = sys.argv[2]
     else:
-        print("Usage: ass2srt ass_file [srt_file]")
+        print('Usage: ass2srt input_file [output_file]')
         return
 
     convert(ass, srt)
+
+
 
 
 if __name__ == '__main__':
