f51cdbbac6bb5a4acf1eba31435a8fe6d7cdf38f
niris <nirisix@gmail.com>
2013-01-16 17:19:02 +0800

    edit gitignore

--- asynclient.py
+++ asynclient.py
@@ -245,6 +245,7 @@ class Request:
 
         self.headers = {
             "User-Agent": "python/asynclient",
+            #"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.29 Safari/537.22",
             "Connection": "close",
             "Accept-Encoding": "identity",
         }
