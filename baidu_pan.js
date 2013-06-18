002089493d9e9adf0f92beb85d0b50f11e9336c4
niris <nirisix@gmail.com>
2013-06-18 15:57:50 +0800

    baidu pan | add url to aria2

--- baidu_pan.js
+++ baidu_pan.js
@@ -1,15 +1,14 @@
 (function() {
-	var result;
-	var dlink = document.querySelector('#downFileButtom');
-	if (dlink !== null) {
-		var name = document.querySelector('h2.b-fl.ellipsis');
-		result = [[name.title, dlink.href]];
-	} else {
-		result = fromDirectory();
-	}
-	output(result);
+	'use strict';
 
-	function fromDirectory() {
+	var result = [];
+	var buttom = document.querySelector('#downFileButtom');
+	if (buttom !== null) {
+		/*one file*/
+		var name = document.querySelector('h2.b-fl.ellipsis');
+		result.push([name.title, buttom.href]);
+	} else {
+		/*directory*/
 		var listUrl = 'http://pan.baidu.com/share/list' +
 			window.location.search +
 			window.location.hash.replace('#dir/path=', '&dir=') +
@@ -17,30 +16,72 @@
 		var xhr = new XMLHttpRequest();
 		xhr.open('get', listUrl, false);
 		xhr.send(null);
-		if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-			response = JSON.parse(xhr.responseText);
-			return response.list.map(function (item) {
-				return [item.server_filename, item.dlink];
+		if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+			var resp = JSON.parse(xhr.responseText);
+			resp.list.forEach(function(elem) {
+				result.push([elem.server_filename, elem.dlink]);
 			});
-		} else {
-			console.log('readState: ' + xhr.readyState + 'status: ' + xhr.status);
-			throw new Error('xhr fail');
 		}
 	}
+	output(result);
+
+/*****************************************************************************/
+
+	var addTasks = function(remote, filelist) {
+		/*use jsonrpc to add tasks*/
+		if (filelist.length === 0) return;
+		var conn = new WebSocket(remote);
+		conn.onopen = function(e) {
+			var data = [];
+			filelist.forEach(function(elem) {
+				data.push({
+					'jsonrpc': '2.0',
+					'id': 'blahblah',
+					'method': 'aria2.addUri',
+					'params': [[elem[1]], {'out': elem[0]}] /* url, name */
+				});
+			});
+			conn.send(JSON.stringify(data));
+			conn.close();
+		};
+	};
 
 	function output(res) {
-		console.log(res);
+		var html = '<form action="">';
+		html += '<label>JSON RPC path: ';
+		html += '<input type="text" value="ws://localhost:6800/jsonrpc" id="remote" />';
+		html += '</label>';
+		res.forEach(function(elem) {
+			html += '<div class="file">';
+			html += '<input type="checkbox" checked />';
+			html += '<label>filename: <input type="text" name="name" value="' + elem[0] + '" /></label>';
+			html += '<a href="' + elem[1] + '">' + elem[0] + '</a>';
+			html += '</div>';
+		});
+		html += '<input type="submit" value="add to aria2" id="submit" />';
+		html += '</form>';
 
-		var list = ['</ul>'];
-		var text = ['</p></div>'];
-		for (var i = res.length - 1; i >= 0; i--) {
-			list.unshift('<li><a href="' + res[i][1] + '">' + res[i][0] + '</a></li>');
-			text.unshift(res[i][1]);
-		}
-		list.unshift('<ul>');
-		text.unshift('<div><p>');
-		var node = '<div>' + list.join('') + text.join('</p><p>') + '</div>';
-		var pop_window = window.open('', 'content', 'height=600, width=600, scrollbars=yes');
-		pop_window.document.body.innerHTML = node;
+		var pop = window.open(
+			'', 'content', 'height=600, width=800, scrollbars=yes');
+		var doc = pop.document;
+		doc.body.innerHTML = html;
+		doc.body.addEventListener('click', function(e) {
+			/*click submit to add tasks*/
+			if (e.target.id === "submit") {
+				e.preventDefault();
+				var remote = doc.querySelector('#remote').value;
+				var filelist = [];
+				var div = doc.querySelectorAll('div.file');
+				for (var i = div.length - 1; i >= 0; --i) {
+					if (div[i].querySelector('input[type=checkbox]').checked) {
+						filelist.push([
+							div[i].querySelector('input[type=text]').value,
+							div[i].querySelector('a').href
+						]);
+					}
+				}
+				addTasks(remote, filelist);
+			}
+		}, false);
 	}
 })();
