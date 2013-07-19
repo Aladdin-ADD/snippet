c611cb24402321809d04b10cffa3cbc391b12af4
niris <nirisix@gmail.com>
2013-07-19 15:44:52 +0800

    update redisSub.py | fix NULL BULK

--- README.rst
+++ README.rst
@@ -1,29 +1,34 @@
 + ass2srt.py 把 ass 字幕转换成 srt 字幕。
 + asynclient.py 异步下载网页。
 + yuedu.py 下载网易云阅读的脚本。
-
++ redisSub.py 无阻塞的 redis subscribe 命令。
 
 
 + bookmarklet
+
+  - baidu_pan.js 百度盘下载地址。
   - github_print.js 用的 bookmarklet。
   - verycd.js 输出 verycd 页面的全部 ed2k 链接，现在没什么用了……
   - yuedu.js 将网易云阅读里的书本内容提取出来。
-  - baidu_pan.js 百度盘下载地址。
 
 
 
 + snippet
+
   - c
+
     + hash.c 很傻的哈希字典。
-    + perfect_hash.c 不会发生碰撞的字典，perfect hash。
-    + skiplist.c 跳跃表，高度居然是随机的。
+    + perfect_hash.c 不会发生碰撞的字典。
+    + skiplist.c 跳跃表，高度随机。
 
   - javascript
+
     + mouse_direction.js 鼠标从哪个方向进入。
     + template.js 一个练手写的的 js 模板，没啥用，有机会再改进。
     + type.js 类型检查。
 
   - python
+
     + chunked.py 处理 chunked 数据。
     + coroutine.py 用于协程的包裹函数。
     + database.py mysql+connector 版的 torndb。
--- redisSub.py
+++ redisSub.py
@@ -52,21 +52,27 @@ class redisSub:
 
     def _read_response(self):
         data = self.fp.readline()
-        flag, response = data[0], data[1:-2]
+        flag, data = data[0], data[1:-2]
         if flag == 36: # b"$" bulk
-            length = int(response) + 2
-            response = self.fp.read(length)
-            response = response[:-2].decode()
+            length = int(data)
+            if length == -1:
+                data = None
+            else:
+                data = self.fp.readline()
+                data = data[:-2].decode()
         elif flag == 42: # b"*" multi bulk
-            length = int(response)
-            response = [self._read_response() for i in range(length)]
+            length = int(data)
+            if length == -1:
+                data = None
+            else:
+                data = [self._read_response() for i in range(length)]
         elif flag == 58: # b":" integer
-            response = int(response)
+            data = int(data)
         elif flag == 43: # b"+" status
-            response = response.decode()
+            data = data.decode()
         elif flag == 45: # b"-" error
-            raise Exception(response.decode())
-        return response
+            raise Exception(data)
+        return data
 
     def subscribe(self, *channels):
         self._command("SUBSCRIBE", *channels)
@@ -94,22 +100,29 @@ class redisSub:
     @gen.coroutine
     def _start_listen(self, callback):
         while True:
-            data = yield gen.Task(self.stream.read_until, b"\r\n")
+            data = yield gen.Task(self.stream.read_until, b"\n")
             flag, data = data[0], data[1:-2]
             if flag != 42:
-                raise Exception("unknown reply flag {}".format(chr(flag)))
-            reply = []
+                raise Exception("error reply flag {}".format(chr(flag)))
             length = int(data)
-            for i in range(length):
-                data = yield gen.Task(self.stream.read_until, b"\r\n")
-                flag, data = data[0], data[1:-2]
-                if flag != 36:
-                    raise Exception("unknown reply flag {}".format(chr(flag)))
-                length = int(data) + 2
-                data = yield gen.Task(self.stream.read_bytes, length)
-                data = data[:-2].decode()
-                reply.append(data)
+            if length == -1:
+                reply = None
+            else:
+                reply = []
+                for i in range(length):
+                    data = yield gen.Task(self.stream.read_until, b"\n")
+                    flag, data = data[0], data[1:-2]
+                    if flag != 36:
+                        raise Exception("err reply flag {}".format(chr(flag)))
+                    length = int(data)
+                    if length == -1:
+                        reply.append(None)
+                    else:
+                        data = yield gen.Task(self.stream.read_until, b"\n")
+                        data = data[:-2].decode()
+                        reply.append(data)
             callback(reply)
+
 
 
 
@@ -117,6 +130,9 @@ class redisSub:
 if __name__ == '__main__':
     from tornado.ioloop import IOLoop
     sub = redisSub()
-    print(sub.subscribe("ch"))
+    print(sub.subscribe("ch", "chan"))
+    print(sub.unsubscribe("chan"))
+    print(sub.psubscribe("chan.*"))
+    print(sub.punsubscribe("ch.*"))
     sub.listen(lambda x: print(x))
     IOLoop.instance().start()
