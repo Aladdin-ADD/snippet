f8ea4ec3291649c90a0efba72c884be419bf6e92
niris <nirisix@gmail.com>
2014-04-23 14:35:13 +0800

    py urlimport

--- snippet/python/urlimport.py
+++ snippet/python/urlimport.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3.3
+#!/usr/bin/env python3.4
 # -*- coding: utf-8 -*-
 
 from urllib import request
-#from types import Moduletype # py3.4
-from imp import new_module as ModuleType
+from types import ModuleType
 import sys
 
 
@@ -12,14 +11,14 @@ def load_module(url):
     source = u.read().decode("utf-8")
     code = compile(source, url, "exec")
     module = sys.modules.setdefault(url, ModuleType(url))
-    module.__file__ = url
+    #module.__name__ = ""
     #module.__package__ = ""
     exec(code, module.__dict__)
     return module
 
 
 if __name__ == '__main__':
-    url = "https://raw.github.com/dhcmrlchtdj/toolkit/master/asynclient.py"
+    url = "https://raw.githubusercontent.com/dhcmrlchtdj/toolkit/master/snippet/python/urlimport.py"
     pkg = load_module(url)
     print(pkg)
     print(dir(pkg))
