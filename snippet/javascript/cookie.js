8dc0644966bfa7124405350f8baab537d79ad043
niris <nirisix@gmail.com>
2014-11-20 16:57:07 +0800

    js cookie | add del

--- snippet/javascript/cookie.js
+++ snippet/javascript/cookie.js
@@ -1,39 +1,51 @@
 (function() {
     "use strict";
 
-    function cookies() {}
-    cookies.set = function(key, val, opt) {
-        var pairs = [key + "=" + encodeURIComponent(val)];
-        if (opt) {
-            if (opt.maxAge) pairs.push("Max-Age=" + opt.maxAge);
-            if (opt.domain) pairs.push("Domain=" + opt.domain);
-            if (opt.path) pairs.push("Path=" + opt.path);
-            if (opt.expires) pairs.push("Expires=" + opt.expires.toUTCString());
-            if (opt.httpOnly) pairs.push("HttpOnly");
-            if (opt.secure) pairs.push("Secure");
-        }
-        var _cookie = pairs.join("; ");
-        document.cookie = _cookie;
-    };
-    cookies.get = function(key) {
-        var _cookie = document.cookie;
-        var pairs = _cookie.split(/; */);
+    var E = encodeURIComponent, D = decodeURIComponent;
+
+    function Cookies() {}
+
+    Cookies.get = function(key) {
+        if (!key) return;
+        var ekey = E(key);
+        var docCookie = document.cookie;
+        var pairs = docCookie.split(/; */);
         for (var i = 0, len = pairs.length; i < len; ++i) {
             var pair = pairs[i];
-            var eq = pair.indexOf("=");
-            if (eq !== -1 && key === pair.substr(0, eq)) {
-                var val = pair.substr(eq + 1);
-                return decodeURIComponent(val);
+            var equal = pair.indexOf("=");
+            if (equal !== -1 && ekey === pair.substr(0, equal)) {
+                return D(pair.substr(equal + 1));
             }
         }
-        return undefined;
     };
 
+    Cookies.set = function(key, value, options) {
+        if (!key) return;
+        var pair = [E(key) + "=" + E(value)];
+        if (options) {
+            if (options.maxAge) pair.push("Max-Age=" + options.maxAge);
+            if (options.domain) pair.push("Domain=" + options.domain);
+            if (options.path) pair.push("Path=" + options.path);
+            if (options.expires) pair.push("Expires=" + options.expires);
+            if (options.httpOnly) pair.push("HttpOnly");
+            if (options.secure) pair.push("Secure");
+        }
+        var cookie = pair.join("; ");
+        document.cookie = cookie;
+    };
+
+    Cookies.del = function(key, options) {
+        if (!key) return;
+        options = options || {};
+        options.expires = "Thu, 01 Jan 1970 00:00:00 GMT";
+        Cookies.set(key, "", options);
+    };
 
 
     if (typeof exports === "object") {
-        module.exports = cookies;
+        module.exports = Cookies;
     } else {
-        window.cookies = cookies;
+        var w = (0, eval)("this");
+        w.Cookies = Cookies;
     }
 })();
