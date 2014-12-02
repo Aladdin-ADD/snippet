c2977ac193664024dfcc84a0a95409bb9da3146d
niris <nirisix@gmail.com>
2014-12-02 13:30:23 +0800

    refine cookie.js

--- snippet/javascript/cookie.js
+++ snippet/javascript/cookie.js
@@ -1,15 +1,13 @@
 (function() {
     "use strict";
 
-    var E = encodeURIComponent, D = decodeURIComponent;
+    var doc = document, E = encodeURIComponent, D = decodeURIComponent;
 
-    function Cookies() {}
-
+    var Cookies = {};
     Cookies.get = function(key) {
         if (!key) return;
         var ekey = E(key);
-        var docCookie = document.cookie;
-        var pairs = docCookie.split(/; */);
+        var pairs = doc.cookie.split(/;\s*/);
         for (var i = 0, len = pairs.length; i < len; ++i) {
             var pair = pairs[i];
             var equal = pair.indexOf("=");
@@ -18,22 +16,23 @@
             }
         }
     };
-
     Cookies.set = function(key, value, options) {
         if (!key) return;
         var pair = [E(key) + "=" + E(value)];
         if (options) {
-            if (options.maxAge) pair.push("Max-Age=" + options.maxAge);
-            if (options.domain) pair.push("Domain=" + options.domain);
             if (options.path) pair.push("Path=" + options.path);
+            if (options.domain) pair.push("Domain=" + options.domain);
+            if (options.maxAge) pair.push("Max-Age=" + options.maxAge);
+            // ie678 not support max-age
+            //var date = new Date(Date.now() + options.maxAge);
+            //pair.push("Expires=" + date.toUTCString());
             if (options.expires) pair.push("Expires=" + options.expires);
-            if (options.httpOnly) pair.push("HttpOnly");
             if (options.secure) pair.push("Secure");
+            if (options.httpOnly) pair.push("HttpOnly");
         }
         var cookie = pair.join("; ");
-        document.cookie = cookie;
+        doc.cookie = cookie;
     };
-
     Cookies.del = function(key, options) {
         if (!key) return;
         options = options || {};
