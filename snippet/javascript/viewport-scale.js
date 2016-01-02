c3b3a9b78041540b3eb0fe3de8f412bb84980814
niris <nirisix@gmail.com>
2016-01-02 13:09:45 +0800

    viewport scale

--- snippet/javascript/viewport-scale.js
+++ snippet/javascript/viewport-scale.js
@@ -1,22 +1,36 @@
 'use strict';
 
-var isIPhone = window.navigator.appVersion.match(/iphone/gi);
-var devicePixelRatio = window.devicePixelRatio;
-var dpr = 1;
-if (isIPhone) {
-    if (devicePixelRatio >= 3) {
-        dpr = 3;
-    } else if (devicePixelRatio >= 2) {
-        dpr = 2;
+var setViewport = function(scale) {
+    var $viewport = document.querySelector('meta[name="viewport"]');
+    if ($viewport) {
+        $viewport.setAttribute('content', 'initial-scale=' + scale);
+    } else {
+        var metaViewport = '<meta name="viewport" content="initial-scale=' + scale + '" />';
+        document.write(metaViewport);
     }
-}
-var scale = 1 / dpr;
+};
 
-var metaViewport = '<meta name="viewport" content="initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no, width=device-width, shrink-to-fit=no" />';
-document.write(metaViewport);
+var updateViewport = function() {
+    var isIPhone = window.navigator.appVersion.match(/iphone/gi);
+    var devicePixelRatio = window.devicePixelRatio;
+    var dpr = 1;
+    if (isIPhone) {
+        if (devicePixelRatio >= 3) {
+            dpr = 3;
+        } else if (devicePixelRatio >= 2) {
+            dpr = 2;
+        }
+    }
 
-var docEl = document.documentElement;
-var width = docEl.getBoundingClientRect().width;
-if (width / dpr > 450) width = dpr * 450;
-var rem = width / 6.4;
-docEl.style.fontSize = rem + 'px';
+    var scale = 1 / dpr;
+    setViewport(scale);
+
+    var docEl = document.documentElement;
+    var width = docEl.getBoundingClientRect().width;
+    if (width / dpr > 450) width = dpr * 450;
+    var rem = width / 6.4;
+    docEl.style.fontSize = rem + 'px';
+};
+
+window.addEventListener('resize', updateViewport);
+updateViewport();
