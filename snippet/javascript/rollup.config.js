34f47767dbe81bbd3d5db5ef459c47f45c979eaa
niris <nirisix@gmail.com>
2016-04-21 23:02:48 +0800

    add rollup config

---
+++ snippet/javascript/rollup.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import replace from 'rollup-plugin-replace';
+import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+var env = process.env.NODE_ENV || 'development';
+
+export default {
+    entry: './src/index.js',
+    dest: './dist/index.js',
+    format: 'cjs',
+    plugins: [
+        json(),
+        babel({
+            presets: [
+                'es2015-rollup'
+            ]
+        }),
+        commonjs({
+            include: 'node_modules/**',
+            extensions: ['.js', '.json'],
+            sourceMap: false
+        }),
+        nodeResolve({
+            extensions: ['.js', '.json']
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    ]
+};
