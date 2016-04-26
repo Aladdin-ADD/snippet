78732f497923066170560c709d756eea5ae18673
niris <nirisix@gmail.com>
2016-04-26 14:59:26 +0800

    update viewport

--- snippet/javascript/viewport-scale2.js
+++ snippet/javascript/viewport-scale2.js
@@ -1,15 +1,18 @@
-'use strict';
-
 // <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"/>
 
-(function() {
+(function(designWidth) {
+    'use strict';
+
     var docEl = document.documentElement;
     var setFontSize = function() {
-        var size = screen.width / 7.5;
-        if (size > 60) size = 60;
+        var width = docEl.clientWidth;
+        var size = Math.ceil(width / designWidth * 100);
+        if (size < 45) size = 45;
+        else if (size > 55) size = 55;
+        while (size % 5 !== 0) size++;
         docEl.style.fontSize = size + 'px';
     };
 
     window.addEventListener('resize', setFontSize);
     setFontSize();
-})();
+})(750);
