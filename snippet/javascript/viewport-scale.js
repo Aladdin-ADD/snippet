b8256a618d9efa02f48667c69e1b10850d2ce0bc
niris <nirisix@gmail.com>
2016-04-28 13:25:14 +0800

    tired

--- snippet/javascript/viewport-scale.js
+++ snippet/javascript/viewport-scale.js
@@ -13,18 +13,18 @@
     var scale = 1 / dpr;
 
     var $viewport = document.querySelector('meta[name="viewport"]');
-    var content = `initial-scale=${scale},maximum-scale=${scale},minimum-scale=${scale},user-scalable=no`;
+    var content = 'initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale + ',user-scalable=no';
     if ($viewport) {
         $viewport.setAttribute('content', content);
     } else {
-        var metaViewport = `<meta name="viewport" content="${content}"/>`;
+        var metaViewport = '<meta name="viewport" content="' + content + '"/>';
         document.write(metaViewport);
     }
 
     var setFontSize = function() {
         var width = docEl.clientWidth;
         if (width / dpr > 450) width = dpr * 450;
-        var fontSize = width / (designWidth / 100);
+        var fontSize = width / designWidth * 100;
         docEl.style.fontSize = fontSize + 'px';
     };
     setFontSize();
