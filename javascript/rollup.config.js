89e444bc6216dcfa9009f9b823c0dc949e11e14a
niris <nirisix@gmail.com>
2016-07-19 16:51:43 +0800

    rollup

--- javascript/rollup.config.js
+++ javascript/rollup.config.js
@@ -1,34 +1,39 @@
-'use strict';
-
-import replace from 'rollup-plugin-replace';
-import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import replace from 'rollup-plugin-replace';
 
 var env = process.env.NODE_ENV || 'development';
+
+var plugins = [
+    nodeResolve(),
+    commonjs({
+        include: 'node_modules/**',
+        extensions: ['.js', '.css'],
+        sourceMap: true,
+    }),
+    json(),
+    replace({
+        'process.env.NODE_ENV': JSON.stringify(env)
+    }),
+    babel({
+        presets: [
+            'es2015-rollup'
+        ]
+    }),
+];
+
+if (env !== 'production') {
+    plugins = [
+        postcss(),
+    ].concat(plugins);
+}
 
 export default {
     entry: './src/index.js',
     dest: './dist/index.js',
     format: 'cjs',
-    plugins: [
-        json(),
-        babel({
-            presets: [
-                'es2015-minimal-rollup'
-            ]
-        }),
-        commonjs({
-            include: 'node_modules/**',
-            extensions: ['.js', '.json'],
-            sourceMap: false
-        }),
-        nodeResolve({
-            extensions: ['.js', '.json']
-        }),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify(env)
-        })
-    ]
+    plugins: plugins,
 };
