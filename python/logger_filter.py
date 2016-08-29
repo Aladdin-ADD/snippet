38f42da6591720db7828a33cd5899c648296f4d0
niris <nirisix@gmail.com>
2016-08-29 09:57:04 +0800

    python logger filter | filter handler

--- python/logger_filter.py
+++ python/logger_filter.py
@@ -2,29 +2,45 @@
 
 import logging
 
-#  disable all logger
-logging.basicConfig(
-    level=logging.NOTSET,
-    handlers=[logging.NullHandler()]
-)
 
-#  custom handler
-formatter = logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(message)s',
-    '%Y-%m-%d %H:%M:%S')
-handler = logging.StreamHandler()
-handler.setLevel(logging.NOTSET)
-handler.setFormatter(formatter)
+def m1(logger_name):
+    filterHandler = logging.StreamHandler()
+    filterHandler.addFilter(logging.Filter(logger_name))
 
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
+    logging.basicConfig(
+        level=logging.NOTSET,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[filterHandler]
+    )
 
+
+def m2(logger_name):
+    logging.basicConfig(
+        level=logging.NOTSET,
+        handlers=[logging.NullHandler()]
+    )
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(message)s',
+        '%Y-%m-%d %H:%M:%S')
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.NOTSET)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(logger_name)
+    logger.addHandler(handler)
+    return logger
 
 if __name__ == '__main__':
-    logger.debug('debug')
+    #  m1('a')
+    #  m2('b')
+
+    logger = logging.getLogger(__name__)
+    logger.warning('from logger')
 
     logger_a = logging.getLogger('a')
-    logger_a.info('info')
+    logger_a.warning('from a')
 
     logger_b = logging.getLogger('b')
-    logger_b.warning('warning')
+    logger_b.warning('from b')
