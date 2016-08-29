6b25651d4ccc3410e1a5ed803651c14c109fa904
niris <nirisix@gmail.com>
2016-08-29 09:42:47 +0800

    python | logger filter | add test

--- python/logger_filter.py
+++ python/logger_filter.py
@@ -10,7 +10,7 @@ logging.basicConfig(
 
 #  custom handler
 formatter = logging.Formatter(
-    '%(asctime)s %(levelname)-8s %(message)s',
+    '%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(message)s',
     '%Y-%m-%d %H:%M:%S')
 handler = logging.StreamHandler()
 handler.setLevel(logging.NOTSET)
@@ -18,3 +18,13 @@ handler.setFormatter(formatter)
 
 logger = logging.getLogger(__name__)
 logger.addHandler(handler)
+
+
+if __name__ == '__main__':
+    logger.debug('debug')
+
+    logger_a = logging.getLogger('a')
+    logger_a.info('info')
+
+    logger_b = logging.getLogger('b')
+    logger_b.warning('warning')
